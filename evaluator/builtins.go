3743d0d4f136d284aeb577b7b0bf3a454aeb8543
Nicolò Santamaria <nicolo.santamaria@gmail.com>
2020-11-25 17:27:22 +0100

    Add append builtin function.

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -6,7 +6,7 @@ var builtins = map[string]*obj.Builtin{
 	"len": &obj.Builtin{
 		Fn: func(args ...obj.Object) obj.Object {
 			if l := len(args); l != 1 {
-				return newError("wrong number of arguments: got %d, want 1", l)
+				return newError("len: wrong number of arguments: got %d, want 1", l)
 			}
 
 			switch arg := args[0].(type) {
@@ -14,9 +14,31 @@ var builtins = map[string]*obj.Builtin{
 			case *obj.String:
 				return &obj.Integer{Value: int64(len(arg.Value))}
 
+			case *obj.Array:
+				return &obj.Integer{Value: int64(len(arg.Elements))}
+
 			default:
 				return newError("len: type not supported, got %s", arg.Type())
 			}
+		},
+	},
+	"append": &obj.Builtin{
+		Fn: func(args ...obj.Object) obj.Object {
+			if len(args) == 0 {
+				return newError("append: no arguments provided")
+			}
+
+			arr, ok := args[0].(*obj.Array)
+			if !ok {
+				return newError("append: first argument must be an array")
+			}
+
+			if len(args) > 1 {
+				for _, a := range args[1:] {
+					arr.Elements = append(arr.Elements, a)
+				}
+			}
+			return arr
 		},
 	},
 }
